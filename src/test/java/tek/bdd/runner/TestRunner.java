e2da4dc7d6ae12dc8b8af19086a158e5e99197af
Morteza <morteza.sharifi.1995@gmail.com>
2024-07-29 21:50:31 -0400

    building the framework

---
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package tek.bdd.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = "", // tags of our scenario
+        features = "classpath:features", // classpath of our feature files
+        glue = "tek.bdd.steps", // package of our steps
+        dryRun = true, // make sure every step has their annotation on it.
+        monochrome = true, // printable console
+        snippets = CucumberOptions.SnippetType.CAMELCASE, // method reading for steps
+        plugin = {
+                // our plugins
+        }
+)
+
+
+public class TestRunner {
+}
