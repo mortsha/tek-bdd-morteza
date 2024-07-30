7c9ffa8981a48d1a9bf004d7e0d96cd20b6c1163
Morteza <morteza.sharifi.1995@gmail.com>
2024-07-30 19:38:31 -0400

    base setup class complete

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,32 @@
+package tek.bdd.base;
+
+import dev.failsafe.internal.util.Durations;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+    private WebDriver driver;
+    public void setupBrowser() {
+        driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+
+
+    }
+
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+
+    public void quitBrowser(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
+
+}
